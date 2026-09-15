2f47cc1af3ab6d81b3f35fee0f6db5a80efd714a
Leonardo <leonardo.n.goettems@gmail.com>
2026-09-15 19:14:30 -0300

    Add student and activity registration features

--- PJ-DevC++-PI2/PJA-PI2.cpp
+++ PJ-DevC++-PI2/PJA-PI2.cpp
@@ -1,4 +1,4 @@
-//Projeto C - Atualizado com Função - PI2
+//Projeto C - Atualizado com Fun??o - PI2
 
 //Bicliotecas
 #include<stdio.h>
@@ -6,13 +6,22 @@
 
 #define mInst 10
 #define mAlu 30
+#define mAt 10
 
 //Vetores
 int vCodInst[mInst];
 char vInstrutor[mInst][50];
 int TLI = 0;
+char vCPFAluno[mAlu][15];
+char vNAluno[mAlu][50];
+int TLA = 0;
+int vCodAt[mAt];
+char vAtividade[mAt][30];
+float vValorAt[mAt];
+int TLAT = 0;
 
-//Funções
+
+//Funcoes
 int buscaInstrutor(int codigo){
 	int i, pos;
 	pos = -1;
@@ -51,8 +60,91 @@ void cadastrarInstrutor(){
 		}
 	}
 }
+int buscaAluno(char cpf[]){
+	int i, pos;
+	pos = -1;
+	
+	for(i = 0; i < TLA; i++)
+	{
+		if(strcmp(vCPFAluno[i], cpf) == 0){
+			pos = i;
+		}
+	}
+	return pos;
+}
 
-//Funções para o Menu
+void cadastrarAluno(){
+	char cpf[15];
+	int pos;
+	
+	getchar();
+	printf("Digite o CPF do alunno: ");
+	gets(cpf);
+	
+	pos = buscaAluno(cpf);
+	
+	if (pos != -1){
+		printf("Aluno ja cadastrado!\n");
+	}
+	else{
+		if (TLA < mAlu){
+			strcpy(vCPFAluno[TLA], cpf);
+			
+			printf("Digite o nome do aluno: ");
+			gets(vNAluno[TLA]);
+			
+			TLA++;
+			printf("Aluno cadastrado com sucesso!\n");
+		}
+		else{
+			printf("Limite de alunos atingido!\n");
+		}
+	}
+}
+
+int buscaAt(int codigo){
+	int i, pos;
+	pos = -1;
+	
+	for(i = 0; i < TLAT; i++){
+		if(vCodAt[i] == codigo){
+			pos = i;
+		}
+	}
+	return pos;
+}
+
+void cadastrarAt(){
+	int codigo, pos;
+	
+	printf("Digite o codigo da atividade: ");
+	scanf("%d", &codigo);
+	
+	pos = buscaAt(codigo);
+	
+	if(pos != -1){
+		printf("Atividade ja cadastrada!\n");
+	}
+	else{
+		if(TLAT < mAt){
+			vCodAt[TLAT] = codigo;
+			
+			getchar();
+			printf("Digite o nome da atividade: ");
+			gets(vAtividade[TLAT]);
+			printf("Digite o valor da atividade: ");
+			scanf("%f", &vValorAt[TLAT]);
+			
+			TLAT++;
+			printf("Atividade cadastrada com sucesso!\n");
+		}
+		else{
+			printf("Limite de atividade atingido!\n");
+		}
+		
+	}
+}
+//Funcoes para o Menu
 int menuPrincipal(){
 	int opcao;
 	
@@ -70,7 +162,9 @@ int menuCadastro(){
 	
 	printf("\n ----- Cadastro -----\n");
 	printf("1 - Cadastrar Instrutor\n");
-	printf("2 - Retornar\n");
+	printf("2 - Cadastrar Aluno\n");
+	printf("3 - Cadastrar Atividade\n");
+	printf("4 - Retornar\n");
 	printf("Escolhar uma Opcao: ");
 	scanf("%d", &opcao);
 	
@@ -82,7 +176,7 @@ int menuCadastro(){
 
 
 
-//Código Principal
+//C?digo Principal
 int main(){
 	int opcaoPrincipal, opcaoCadastro;
 	
@@ -91,9 +185,15 @@ int main(){
 	while(opcaoPrincipal != 2){
 		if(opcaoPrincipal == 1){
 			opcaoCadastro = menuCadastro();
-			while(opcaoCadastro != 2){
+			while(opcaoCadastro != 4){
 				if(opcaoCadastro == 1){
 					cadastrarInstrutor();
+				}
+				else if(opcaoCadastro == 2){
+					cadastrarAluno();
+				}
+				else if(opcaoCadastro == 3){
+					cadastrarAt();
 				}
 				opcaoCadastro = menuCadastro();
 			}
@@ -102,4 +202,3 @@ int main(){
 	}
 	printf("\nPrograma Encerrado.\n");
 }
-
